1ab5a041190d338b83ad04df2a334fa9d2d07fd1
GIIIPFEL <kindlerherbert@gmail.com>
2022-07-03 22:38:10 +0200

    progress

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,8 +1,30 @@
-import { useLayoutEffect } from 'react'
-import { StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+
+
+
+
+
+// https://youtu.be/MJzmZ9qmdaE?t=10413
+
+
+
+
+
+
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import { faPhone, faVideo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { useLayoutEffect, useState } from 'react'
+import { Keyboard, ScrollView, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, KeyboardAvoidingView, Platform } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { TouchableWithoutFeedback } from 'react-native-web'
+import firebase from 'firebase/compat/app';
+import { db, auth } from '../firebase'
 
 const ChatScreen = ({ navigation, route }) => {
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([])
+
     useLayoutEffect(() => {
 
         navigation.setOptions({
@@ -21,23 +43,74 @@ const ChatScreen = ({ navigation, route }) => {
                 </View>
             ),
             headerRight: () => (
-                <View>
-                    <TouchableHighlight>
-                        <FontAwesome name="video-camera" size={24} color='#000' />
-                    </TouchableHighlight>
-                    <TouchableHighlight>
-                        <FontAwesome name="call" size={24} color='#000' />
-                    </TouchableHighlight>
+                <View style={[styles.headerContainer, { justifyContent: 'space-evenly', width: 120 }]}>
+                    <TouchableOpacity>
+                        <FontAwesomeIcon icon={faVideo} size={24} color='#000' />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <FontAwesomeIcon icon={faPhone} size={24} color='#000' />
+                    </TouchableOpacity>
                 </View>
             )
         })
 
     }, [navigation])
 
+    const sendMessage = () => {
+        Keyboard.dismiss();
+
+        db.collection('chats').doc(route.params.id).collection('messages').add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL,
+        })
+
+        setInput('');
+    }
+
+    useLayoutEffect(() => {
+        return db.collection('chats').doc(route.params.id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => setMessages(
+            snapshot.docs.maps(doc => {
+                id: doc.id,
+                    data: doc.data()
+            })
+        ))
+    }, [route])
+
     return (
-        <View>
-            <Text>ChatScreen</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : ''} style={styles.container} keyboardVerticalOffset={90}>
+                <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+
+                    <>
+
+                        <ScrollView>
+                            {messages.map(({ id, data }) => (
+                                data.email === auth.currentUser.email ? (
+                                    <View>
+                                        <Avatar />
+                                        <Text></Text>
+                                    </View>
+                                ) : (
+                                    <View></View>
+                                )
+                            )}
+                        </ScrollView>
+
+                        <View style={styles.footer}>
+                            <TextInput style={styles.textInput} placeholder='Enter Message' value={input} onChangeText={text => setInput(text)} onSubmitEditing={() => sendMessage()} />
+                            <TouchableOpacity activeOpacity={0.5} onPress={() => sendMessage()}>
+                                <FontAwesomeIcon icon={faPaperPlane} size={28} color='#2b68e6' />
+                            </TouchableOpacity>
+                        </View>
+
+                    </>
+
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
@@ -49,8 +122,29 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     chatName: {
-        marginLeft: 10,
+        marginLeft: 20,
         fontWeight: '700',
         fontSize: 20,
-    }
+    },
+    container: {
+        flex: 1,
+    },
+    footer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        padding: 15,
+    },
+    textInput: {
+        bottom: 0,
+        height: 40,
+        flex: 1,
+        marginRight: 15,
+        backgroundColor: '#f3f3f3',
+        padding: 10,
+        paddingLeft: 20,
+        color: '#000',
+        borderRadius: 30,
+    },
 })
